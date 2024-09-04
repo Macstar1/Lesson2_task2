c01640bc5e4eef92342eb1c9c57f194da7c2a590
maopa <maopa@bk.ru>
2024-09-04 17:34:59 +0300

    Move function

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,16 +1,16 @@
 fun main() {
-    val likes = 121
-
-    fun people(likes: Int): String {
-        return if (likes == 1 || (likes % 10 == 1) &&
-            (likes % 100 != 11) &&
-            likes != 11
-        ) {
-            "человеку"
-        } else {
-            "людям"
-        }
-    }
+    val likes = 111
 
     println("Понравилось $likes ${people(likes)}")
+}
+
+fun people(likes: Int): String {
+    return if (likes == 1 || (likes % 10 == 1) &&
+        (likes % 100 != 11) &&
+        (likes != 11)
+    ) {
+        "человеку"
+    } else {
+        "людям"
+    }
 }
