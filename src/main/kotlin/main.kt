b9eeb96402bf67cf1d303d3c566a05061739f7d6
maopa <maopa@bk.ru>
2024-09-04 16:08:09 +0300

    Code format

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,15 +1,16 @@
 fun main() {
     val likes = 121
 
-    fun people(likes: Int):String {
+    fun people(likes: Int): String {
         if (likes == 1 || (likes % 10 == 1) &&
             (likes % 100 != 11) &&
-            likes != 11){
+            likes != 11
+        ) {
             return "человеку"
-        }
-        else{
+        } else {
             return "людям"
         }
     }
+
     println("Понравилось $likes ${people(likes)}")
 }
